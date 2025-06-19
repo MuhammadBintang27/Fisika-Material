c894150047c53facc6ca8ac8ef7e61ebdc764f6e
MuhammadBintang27 <muhammad.bintang.indra@gmail.com>
2025-06-19 13:30:06 +0700

    feat: adjust on home feature

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,9 @@
+{{-- resources/views/home.blade.php --}}
 @extends('layouts.app')
-
 @section('title', 'Beranda - Laboratorium Fisika Dasar')
-
 @section('content')
     @include('components.hero')
     @include('components.about')
-    @include('components.article')
+    @include('components.articles')
     @include('components.laboratorium')
 @endsection
